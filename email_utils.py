915d879baedd478f8508dee9609660612d192fef
Kaio Pablo Carvalho Mendes <kaiopablo17@gmail.com>
2025-06-13 23:10:01 -0300

    Protecao do email e senha e implementacao da confirmacao de agendamento

--- email_utils.py
+++ email_utils.py
@@ -1,5 +1,5 @@
 # email_utils.py
-
+import streamlit as st
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -8,8 +8,9 @@ import os
 
 load_dotenv()  # lê o arquivo .env
 
-EMAIL_REMETENTE = os.getenv("EMAIL_REMETENTE")
-SENHA_REMETENTE = os.getenv("SENHA_REMETENTE")
+EMAIL_REMETENTE = st.secrets["EMAIL"]["REMETENTE"]
+SENHA_REMETENTE = st.secrets["EMAIL"]["SENHA"]
+
   
 
 def enviar_email_promocional(destinatario, nome_cliente, nome_pet=None):
@@ -35,3 +36,21 @@ def enviar_email_promocional(destinatario, nome_cliente, nome_pet=None):
             servidor.sendmail(EMAIL_REMETENTE, destinatario, mensagem.as_string())
     except Exception as e:
         print(f"Erro ao enviar e-mail: {e}")
+
+def enviar_email_confirmacao_agendamento(email, nome, servico, data, hora):
+    corpo = f"""
+    Olá {nome},
+
+    Seu agendamento para o serviço '{servico}' foi confirmado para o dia {data} às {hora}.
+
+    Agradecemos por escolher o nosso Pet Shop! 🐾
+    """
+    msg = MIMEText(corpo)
+    msg['Subject'] = "Confirmação de Agendamento - Pet Shop"
+    msg['From'] = "seu_email@gmail.com"
+    msg['To'] = email
+
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.login(EMAIL_REMETENTE, SENHA_REMETENTE)
+        smtp.send_message(msg)
